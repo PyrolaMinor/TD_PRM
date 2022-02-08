e022b289a47a71b8a6a07c804ebde772570b7a90
Mathis <mathis.chesnel@gmail.com>
2022-02-08 09:53:40 +0100

    Ajout de la base avec insertion/update nbrePassage, faire attention au info de connexion/nombase.

--- TD/index.js
+++ TD/index.js
@@ -8,11 +8,14 @@ const app = express();
 const port = 3000;
 
 const db = mysql.createConnection({
+    database: "td2_prm",
     host : "localhost",
     user: "root",
-    password: "root"
+    password: ""
 });
-
+db.connect(function(err){
+  if(err) throw err;
+});
 // Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -28,13 +31,35 @@ app.get('/', (req, res) => {
 
 // POST client data to file
 app.post('/saveClientData', (req, res) => {
-    db.connect(function(err) {
+    /*db.connect(function(err) {
         if (err) throw err;
         console.log("Connecté à la base de données MySql!");
-    });
+    });*/
     let fingerprintHashed = req.body.fingerprintHashed;
     let fingerprint = req.body.fingerprint;
-
+    console.log(fingerprintHashed);
+    var select = "SELECT * FROM tad where code_client ='"+fingerprintHashed+"';";
+    db.query(select, function(err, results){
+      if (err) throw err;
+      console.log(results)
+      console.log(results.length)
+      if(results.length != 0){
+        var nbPassage = parseInt(results[0].nbrePassage, 10)
+        var update = "UPDATE tad SET nbrePassage = "+ (nbPassage+1)+"  where code_client ='"+fingerprintHashed+"';";
+        db.query(update, function(err, results){
+          if (err) throw err;
+          console.log("J'ai bien update sa mère ! ");
+        });
+      }else{
+        var insert = "INSERT INTO `tad` (`code_client`, `fingerprint`, `horodatage`, `nbrePassage`) VALUES ('"+fingerprintHashed+"', '"+fingerprint+"', '"+new Date()+"', '"+1+"');";
+        db.query(insert, function(err, results){
+          if (err) throw err;
+          console.log("J'ai bien insérer ! ");
+        });
+      }
+    });
+    
+    
     let savedData = [fingerprintHashed, fingerprint].join(",")+"\n";
 
     fs.appendFile('infos.txt', savedData, function (err) {
@@ -42,6 +67,7 @@ app.post('/saveClientData', (req, res) => {
       console.log('Saved client data', req.body);
     });
     res.sendStatus(200);
+
 });
 
 app.listen(port, () => {
